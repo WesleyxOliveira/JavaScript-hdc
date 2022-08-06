91b468e28526fb38d87286f9822f45ffdd628bd3
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-06 14:11:46 -0300

    continuação seção 08

---
+++ projetos/secao 08 arrays e objetos/04_metodos/script.js
@@ -0,0 +1,12 @@
+let marca = "Nike";
+
+console.log(marca.toUpperCase()); // metodo
+
+let marca2 = marca.toUpperCase();
+
+console.log(marca2.toLocaleLowerCase());
+
+console.log(typeof marca2.toLocaleLowerCase);
+
+// string.propriedade
+// string.metodo()

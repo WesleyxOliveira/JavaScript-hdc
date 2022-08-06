18bfae49dfb3bf6ecb749e1ce68ddc9266a32e83
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-06 13:01:26 -0300

    criei seção 08

---
+++ projetos/secao 08 arrays e objetos/02_mais_sobre_arrays/script.js
@@ -0,0 +1,8 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ["Wesley", "João", "Aléxa"];
+let bool = [true, false, true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(nomes[nomes.length-1]);

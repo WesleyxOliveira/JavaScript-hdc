de3abe5a58b5b7080fb9654d7bdc4e89026f72e7
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-10 19:16:56 -0300

    atualizei a seção 08

---
+++ projetos/secao 08 arrays e objetos/08_keys/script.js
@@ -0,0 +1,12 @@
+// Object.keys(object_name) retorna o nome das chaves/propriedades dos objetos.
+
+let obj = {
+    'chave1': 1,
+    'chave2': 2,
+    'chave3': 3,
+}
+
+console.log(obj);
+
+console.log(Object.keys(obj));
+

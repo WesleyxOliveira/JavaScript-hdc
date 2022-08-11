2bd39e337cc4abd2ec5ab4b571db0c694042a85e
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-10 21:03:16 -0300

    atualizei seção 08

---
+++ projetos/secao 08 arrays e objetos/09_mutacao/script.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome: "Wesley",
+};
+
+let pessoa2 = pessoa;
+let pessoa3 =  {
+    nome: "Wesley",
+}
+
+console.log(pessoa == pessoa2);
+
+pessoa2.nome = "Pedro";
+
+console.log(pessoa.nome);
+
+pessoa.nome = "Maria";
+
+console.log(pessoa2.nome);

3f29aa80c0d7db1598e055caffbc3a0dc0307b69
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-18 14:04:34 -0300

    atualizei a seção 08

---
+++ projetos/secao 08 arrays e objetos/25_destructuring_array/script.js
@@ -0,0 +1,11 @@
+/*
+ destructuring serve para desestruturar o obj ou array em variáveis, tornando o acesso aos valores mais fácil.
+*/
+
+let numeros = [2, 4, 5, 8];
+
+let [num1, num2, num3, num4] = numeros;
+
+console.log(num1);
+console.log(num3);
+console.log(num4);

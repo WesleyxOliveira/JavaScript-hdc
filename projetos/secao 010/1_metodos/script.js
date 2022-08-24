5a9f103d6101d6fcaa03396c8f5c5f8a3524964d
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-08-23 21:18:42 -0300

    comecei seção 9

---
+++ projetos/secao 010/1_metodos/script.js
@@ -0,0 +1,11 @@
+const  cachorro = {
+    uivar: function() {
+        console.log("Auuuuuuuuuuu");
+    }, 
+    rosnar: function() {
+        console.log("grrrrrrr");
+    }
+}
+
+cachorro.uivar();
+cachorro.rosnar();

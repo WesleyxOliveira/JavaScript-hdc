ab3353ff606fe7d816449528e5bcaf1c203c2235
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-09-04 10:32:47 -0300

    ex02 OO(carrinho de compras)

--- projetos/secao 011 exercicio oo/2_exercicio_37/script.js
+++ projetos/secao 011 exercicio oo/2_exercicio_37/script.js
@@ -12,10 +12,32 @@ class Carrinho {
         for(let itemCarrinho in this.itens) {
             if(this.itens[itemCarrinho].id == item.id) {
                 this.itens[itemCarrinho].qtd += item.qtd;
+                contador = 1;
             }
         }
+
+        if(contador == 0) {
+            this.itens.push(item);
+        }
+
         this.qtd += item.qtd;
         this.valorTotal += item.preco * item.qtd;
+    }
+
+    removeItem(item) {
+        for(let itemCarrinho in this.itens) {
+            if(this.itens[itemCarrinho].id == item.id) {
+                
+                let obj = this.itens[itemCarrinho];
+                let index = this.itens.findIndex(function(obj){return obj.id == item.id});
+
+                this.qtd -= this.itens[itemCarrinho].qtd;
+                this.valorTotal -= this.itens[itemCarrinho].preco * this.itens[itemCarrinho].qtd;
+
+                this.itens.splice(index, 1);
+
+            }
+        }
     }
 }
 
@@ -38,5 +60,13 @@ let carrinho = new Carrinho([
 console.log(carrinho);
 
 carrinho.additem({id: 01, nome: "Camisa", qtd: 2, preco: 20});
+
+console.log(carrinho);
+
+carrinho.additem({id: 03, nome: "Boné", qtd: 1, preco: 15});
+
+console.log(carrinho);
+
+carrinho.removeItem({id: 01, nome: "Camisa", qtd: 2, preco: 20});
 
 console.log(carrinho);

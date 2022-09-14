797faf5fcdb40da2404c70b9260d6b2d9edd2e18
Wesley De Souza Oliveira <Wesleyxoliveira@gmail.com>
2022-09-14 11:08:33 -0300

    iniciei a seçao debugs do curso

---
+++ projetos/secao 012 debug e erros/01_strict_mode/script.js
@@ -0,0 +1,16 @@
+// "use strict"
+
+let opa = "teste";
+
+// delete Object.prototype;
+
+function teste() {
+    "use strict"
+    let testando = "teste";
+}
+
+teste();
+
+let o = {p:1, p:2};
+
+console.log(p)
